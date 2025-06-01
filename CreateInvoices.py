1fb43e0650cef73999d2737a29c73eaa4ea767cc
SnejPro <11321022+SnejPro@users.noreply.github.com>
2025-06-01 21:43:54 +0000

    Add Script to automatically add invoices to bookings
    Signed-off-by: SnejPro <11321022+SnejPro@users.noreply.github.com>

---
+++ CreateInvoices.py
@@ -0,0 +1,59 @@
+#!PyVenv/bin/python3
+
+import functions
+from functions import *
+import datetime
+import json
+import os
+from pprint import pprint
+import requests
+import time
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog='EasyVereinScripts - CreateInvoices',
+    description='Create Invoices for all bookings for a given time period',
+    add_help=True
+)
+parser.add_argument(
+    '--date__gte',
+    type=lambda s: datetime.datetime.strptime(s, '%Y-%m-%d')
+)
+parser.add_argument(
+    '--date__lte',
+    type=lambda s: datetime.datetime.strptime(s, '%Y-%m-%d')
+)
+args=parser.parse_args()
+
+config=functions.config_get()
+easy_verein=easy_verein(
+    api_key=config["EasyVerein"]["ApiKey"],
+)
+
+bookings=easy_verein.bookings_get(
+    date__gte=args.date__gte,
+    date__lte=args.date__lte,
+    relatedInvoice__isnull=True
+)
+
+current_time=datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+with open("bkp/bookings_%s_.json" % current_time, "w") as booking_bkp:
+    json.dump(bookings, booking_bkp, indent=4)
+
+i=0
+for booking in bookings:
+    if (i>=1):
+        break
+
+    invoice=easy_verein.invoice_create(
+        amount=booking["amount"],
+        date=booking["date"],
+        receiver=booking["receiver"],
+        invNumber="auto_%s_%s" % (current_time, i),
+        relatedBooking=booking["id"],
+        booking=booking,
+    )
+    print(invoice)
+
+    i=i+1
--- functions.py
+++ functions.py
@@ -2,6 +2,7 @@ import json
 import datetime
 import os
 import requests
+import re
 
 def selective_merge(base_obj, delta_obj):
     if not isinstance(base_obj, dict):
@@ -49,9 +50,10 @@ class last_call():
 class easy_verein():
     billing_accounts= {}
 
-    def __init__(self, api_key, bank_account):
+    def __init__(self, api_key, bank_account=None):
         self.headers = {"Authorization": "Bearer %s" % api_key}
-        self.bank_account=bank_account
+        if bank_account!=None:
+            self.bank_account=bank_account
 
     def billing_account_get(self, number):
         if number in self.billing_accounts:
@@ -70,6 +72,38 @@ class easy_verein():
             return response.json()["results"][0]["id"]
         else:
             raise Exception("Billing account number %s could not be found" % (number))
+        
+    def bookings_get_fetch_next(self, previous_response, results=[]):
+        if "next" in previous_response and previous_response["next"]!=None:
+            response = requests.get(
+                previous_response["next"],
+                headers=self.headers
+            )
+            response_content=response.json()
+            results=response_content["results"]
+            results.extend(self.bookings_get_fetch_next(previous_response=response_content))
+
+        return results
+
+    def bookings_get(self, date__gte, date__lte, relatedInvoice__isnull=None):
+        params={
+            "date__gt": date__gte-datetime.timedelta(seconds=1),
+            "date__lt": date__lte+datetime.timedelta(days=1),
+            "limit": 100,
+            "ordering": "date"
+        }
+        if relatedInvoice__isnull!=None:
+            params["relatedInvoice__isnull"]=relatedInvoice__isnull
+
+        response = requests.get(
+            'https://easyverein.com/api/v2.0/booking',
+            params=params,
+            headers=self.headers
+        )
+        response_content=response.json()
+        results=response_content["results"]
+        results.extend(self.bookings_get_fetch_next(previous_response=response_content))
+        return results
 
     def booking_id_exists(self, id):
         response = requests.get(
@@ -83,6 +117,8 @@ class easy_verein():
             return True
 
     def booking_create(self, transaction):
+        if self.bank_account==None:
+            raise Exception("Class easy_verein was called without parameter bank_account")
         if not self.booking_id_exists(transaction["billingId"]):
             print("Transaction '%s' does not exist. Creating ..." % transaction["billingId"])
             data = transaction
@@ -98,3 +134,80 @@ class easy_verein():
                 raise Exception("Error creating Transaction %s:\n%s" % (transaction["billingId"], response.json()))
         else:
             print("Transaction '%s' already exists. Skipping" % transaction["billingId"])
+
+    def invoice_create(
+        self,
+        amount,
+        date,
+        invNumber,
+        receiver="**Missing**",
+        relatedBooking=None,
+        booking=None,
+    ):
+        if float(amount) < 0:
+            kind="expense"
+        else:
+            kind="revenue"
+        amount_absolut=abs(float(amount))
+
+        data={
+            "charges": {
+                "total": amount_absolut
+            },
+            "date": re.search("^\d{4}-\d{2}-\d{2}", date).group(),
+            "receiver": receiver,
+            "kind": kind,
+            "totalPrice": amount_absolut,
+            "invNumber": invNumber
+        }
+        if relatedBooking!=None:
+            data["relatedBookings"] = [ relatedBooking ]
+        if receiver==None or receiver=="":
+            data["receiver"] = "**Missing**"
+
+        invoice_response = requests.post(
+            'https://easyverein.com/api/v2.0/invoice',
+            json=data,
+            headers=self.headers
+        )
+        invoice=invoice_response.json()
+        if invoice_response.status_code!=201:
+            raise Exception("Error creating invoice object: %s\nresponse: %s" % data, relatedBooking, invoice_response.json())
+
+        data={
+            "relatedBookings": [ relatedBooking ]
+        }
+
+        invoice_patch_response = requests.patch(
+            'https://easyverein.com/api/v2.0/invoice/%s' % invoice["id"],
+            json=data,
+            headers=self.headers
+        )
+        invoice_patch=invoice_patch_response.json()
+        if invoice_patch_response.status_code!=200:
+            raise Exception("Error combining invoice with booking invoice: %s, booking: %s\nresponse: %s" % (invoice["id"], relatedBooking, invoice_patch_response.json()))
+
+        # data={
+        #     "assign_all_open": False,
+        #     "billingAccount": re.search("\d+$", booking["billingAccount"]).group(),
+        #     "bookingId": relatedBooking,
+        #     "debit_call_data": "",
+        #     "dry": False,
+        #     "invoices": {
+        #         invoice["id"]: True
+        #     },
+        #     "remove_all": False,
+        #     "sphere": booking["sphere"],
+        #     "useTabledataFormat": True
+        # }
+        # if booking["bookingProject"]!=None:
+        #     data["groupId"]=re.search("\d+$", booking["bookingProject"]).group()
+
+        # booking_response = requests.post(
+        #     'https://easyverein.com/app/api/combineBookingAndInvoice/',
+        #     data=data,
+        #     headers=self.headers
+        # )
+        # print(booking_response.content)
+
+        return invoice_patch
